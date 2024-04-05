6cc503a2aae084d37a3fc9048fae3b550c0a885e
Natalia Espinosa <nataliage18@gmail.com>
2024-04-05 15:39:29 -0600

    Se agrega el proyecto

---
+++ About me/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  About me
+//
+//  Created by Alumno on 01/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
